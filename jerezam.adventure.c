8525f317e4d7f97b3484e5e01a570d943e6ebfa9
MichaelJereza <mdj1999@gmail.com>
2019-11-01 16:13:52 -0700

    just need to add multithreading

---
+++ jerezam.adventure.c
@@ -0,0 +1,272 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <pthread.h>
+
+struct Room {
+    char* name;
+    int doors; //count of connections
+    struct Room* connections[6];
+    char* type;
+};
+
+// Searches through directory, finding newest one
+// Citing code from Lec 1.6 slide 6
+void getNewestDir(char* output_buffer){
+    int time = -1;
+    char prefix[] = "jerezam.rooms.";
+    char dirName[60];
+    memset (dirName, '\0', sizeof(dirName)); // Initializes string with null terminator
+    
+    DIR* checkDir;
+    struct dirent* current_file;
+    struct stat attr;
+
+    checkDir = opendir(".");
+
+    while((current_file = readdir(checkDir)) != NULL){
+        if(strstr(current_file->d_name, prefix) != NULL){ //if prefix
+            stat(current_file->d_name, &attr);
+            if (attr.st_mtime > time){
+                time = attr.st_mtime;
+                memset (dirName, '\0', sizeof(dirName)); // Reinitialize string each time
+                strcpy(dirName, current_file->d_name);
+            }
+        }
+    }
+    closedir(checkDir);
+    strcpy(output_buffer, dirName);
+    return;
+}
+
+// Generate Room pointer array to dynamically allocated memory
+// Contents from files ROOM_[1-7]
+struct Room** getRooms(){
+    struct Room** rooms;
+    char buffer[30];
+    size_t bfsz = 0;
+    char* lineptr = NULL;
+    char room_number;
+   
+    char directory[30];
+    memset (directory, '\0', sizeof(directory));
+
+    getNewestDir(directory);
+    
+    rooms = malloc(7*sizeof(struct Room*));
+
+    for(int i = 0; i < 7; i++){
+        // Create memory for room[i]
+        rooms[i] = malloc(sizeof(struct Room));
+        rooms[i]->name = malloc(sizeof(char[8]));
+        rooms[i]->doors = 0;
+        
+        // Directory path into buffer
+        memset (buffer, '\0', sizeof(buffer));
+        strcpy(buffer, directory);
+
+        // File path into buffer
+        strcat(buffer, "/ROOM_");
+        room_number = (i+1) + '0';
+        strncat(buffer, &room_number, 1);
+
+        FILE* room_stream = fopen(buffer, "r"); //using buffer as the file path
+
+        // Set position of beginning of name
+        int pos = fseek(room_stream, 11, SEEK_CUR);
+        getline(&lineptr, &bfsz, room_stream);
+
+        // Find newline and add null terminator
+        for(int newline = 0; newline < bfsz; newline++){
+            if (lineptr[newline]=='\n'){
+                lineptr[newline]='\0';
+            }
+        }
+        // Add name to rooms[i]
+        // and free lineptr
+        strcpy(rooms[i]->name, lineptr);
+        bfsz = 0;
+        fclose(room_stream);
+        free(lineptr);
+
+    }
+    // Rooms allocated and have names
+    
+    // Now add connections and types
+    for(int c = 0; c < 7; c++){
+        // Directory path into buffer
+        memset (buffer, '\0', sizeof(buffer));
+        strcpy(buffer, directory);
+        
+        // File path into buffer
+        strcat(buffer, "/ROOM_");
+        room_number = (c+1) + '0';
+        strncat(buffer, &room_number, 1);
+
+        FILE* room_stream = fopen(buffer, "r"); //using buffer as the file path        
+        
+        ssize_t read;
+        int con_num = 0; // Iterates based on connections number
+        while(read = getline(&lineptr, &bfsz, room_stream) != -1){
+            
+            // Set connection number
+            if((lineptr[11] == '1')||(lineptr[11] == '2')||(lineptr[11] == '3')||(lineptr[11] == '4')||(lineptr[11] == '5')||(lineptr[11] == '6')){
+                con_num = lineptr[11]-'0';
+            }
+
+            // If line is a connection
+            if(con_num!=0){
+                // For end of format to end of name
+                for(int iter = 14; iter < strlen(lineptr); iter++){
+                    // If iter is last char
+                    // Append \0
+                    if(iter == strlen(lineptr)-1){
+                        buffer[iter-14] = '\0';
+
+
+                    }
+                    else{
+                        buffer[iter-14] = lineptr[iter];
+                    }
+                }     
+                // Name from string in buffer
+                char name[8];
+                struct Room* sought = NULL;
+                strcpy(name, buffer);
+                
+                // For connections, if name, pointer to room in sought
+                for(int room_seek = 0; room_seek < 7; room_seek++){
+                    if(strcmp(rooms[room_seek]->name, name)==0){
+                        sought = rooms[room_seek];
+                        room_seek=7;
+                    }
+                }
+                // Add sought to connections
+                rooms[c]->connections[con_num-1] = sought;
+                rooms[c]->doors++;
+                con_num = 0;
+            }
+            // If line is type
+            else if(lineptr[5]=='T'){
+                for(int typ_iter = 11; typ_iter <= strlen(lineptr); typ_iter++){
+                    // Append \0
+                    if(typ_iter == strlen(lineptr)){
+                        buffer[typ_iter-11] = '\0';
+                    }
+                    else{
+                        buffer[typ_iter-11] = lineptr[typ_iter];
+                    }
+                }
+                // Add type to room
+                rooms[c]->type = malloc(sizeof(char[8]));
+                strcpy(rooms[c]->type, buffer);
+            }  
+        }
+        fclose(room_stream);
+        free(lineptr);
+    }
+    return rooms;
+}
+
+// Deallocates memory used by room array.
+void freeRooms(struct Room** rooms){
+    for(int i = 0; i < 7; i++){
+     //   printf("%s, %s, %d, %s\n", rooms[i]->name, rooms[i]->connections[0]->name, rooms[i]->doors, rooms[i]->type);
+        free(rooms[i]->name);
+        free(rooms[i]->type);
+        free(rooms[i]);
+    }
+    free(rooms);
+    return;
+}
+
+void prompt(struct Room* location){
+    printf("\nCURRENT LOCATION: %s\n", location->name);
+    printf("POSSIBLE CONNECTIONS: ");
+    for(int d = 0; d < location->doors; d++){
+        printf("%s", location->connections[d]->name);
+        if(d == location->doors-1){
+            printf(".\n");
+        }
+        else{
+            printf(", ");
+        }
+    }
+    printf("WHERE TO? >");
+    return;
+}
+
+char* getValidInput(struct Room* location){
+    char* buffer = NULL;
+    size_t bfsz = 0;
+    bool valid = false;
+    do{
+        prompt(location);         
+
+        getline(&buffer, &bfsz, stdin);
+        buffer[strlen(buffer)-1]='\0';
+
+        if(strcmp(buffer, "time")==0){
+            valid = true;
+            break;
+        }
+        else{
+            bool huh = true;
+            for(int con = 0; con < location->doors; con++){
+                if(strcmp(buffer, location->connections[con]->name)==0){
+                    valid = true;
+                    huh = false;
+                    break;
+                }
+            }
+            if(huh){
+                printf("\nHUH? I DON’T UNDERSTAND THAT ROOM. TRY AGAIN.\n");
+            }
+        }
+    }while(!valid);
+    return buffer;
+}
+
+void getTime(){
+
+}
+
+void play(struct Room** map){
+    struct Room* cur_room = map[0];
+    char** history = NULL;
+    int steps = 0;
+    char* choice = NULL;
+    while(strcmp(cur_room->type, "END_ROOM")!=0){
+        choice = getValidInput(cur_room);
+        if(strcmp(choice, "time")==0){
+            getTime();
+        }
+        else{
+            for(int con = 0; con < cur_room->doors; con++){
+                if(strcmp(choice, cur_room->connections[con]->name)==0){
+                    cur_room = cur_room->connections[con];
+                    history = realloc(history, (steps+1)*sizeof(history[8]));
+                    history[steps] = cur_room->name;
+                    steps++;
+                }
+            }
+        }
+        free(choice);
+    }
+    printf("\nYOU HAVE FOUND THE END ROOM. CONGRATULATIONS!\nYOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", steps);
+    for(int s = 0; s < steps; s++){
+        printf("%s\n", history[s]);
+    }
+    free(history);
+}
+
+void main(){
+    struct Room** the_rooms = getRooms();
+    play(the_rooms);
+
+    freeRooms(the_rooms);
+}
--- jerezam.buildrooms.c
+++ jerezam.buildrooms.c
@@ -1,87 +1,206 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
-char* names[10] = {
-	"bleak",
-	"bitter",
-	"branding",
-	"tainted",
-	"eldritch",
-	"haunted",
-	"sentient",
-	"macabre",
-	"risky"
-};
+#include <time.h>
+#include <stdbool.h>
+#include <fcntl.h>
 
 struct Room {
   char* name;
-  struct Room* connections;
+  int doors; //count of connections
+  struct Room* connections[6];
   char* type;
-}
+};
 
 // Returns true if all rooms have 3 to 6 outbound connections, false otherwise
-bool IsGraphFull()  
+bool IsGraphFull(struct Room** all_rooms)  
 {
-  ...
+  for(int i=0; i<7; i++){
+    int c=all_rooms[i]->doors;
+    if((c<3)||(c>6)){
+      return false;
+    }
+  }
+  return true;
 }
 
-// Adds a random, valid outbound connection from a Room to another Room
-void AddRandomConnection()  
+
+// Returns a random Room, does NOT validate if connection can be added
+struct Room* GetRandomRoom(struct Room** array)
 {
-  Room A;  // Maybe a struct, maybe global arrays of ints
-  Room B;
+  int l = rand() % 7;
+  return (array[l]);
+}
+
+// Returns true if a connection can be added from Room x (< 6 outbound connections), false otherwise
+bool CanAddConnectionFrom(struct Room x) 
+{
+  if(x.doors < 6){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+// Returns true if a connection from Room x to Room y already exists, false otherwise
+bool ConnectionAlreadyExists(struct Room* x, struct Room* y)
+{
+  for(int i = 0; i < x->doors; i++){
+    if(x->connections[i]->name==y->name){
+      return true;
+    }
+  }
+    return false;
+}
+
+// Connects Rooms x and y together, does not check if this connection is valid
+void ConnectRoom(struct Room* x, struct Room* y) 
+{
+  if(x->doors == 0){
+    x->connections[0] = y;
+  }
+  else{
+    x->connections[x->doors] = y;
+  }
+  x->doors++;
+}
+
+// Returns true if Rooms x and y are the same Room, false otherwise
+bool IsSameRoom(struct Room x, struct Room y) 
+{
+  if(x.name==y.name){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+
+// Adds a random, valid outbound connection from a Room to another Room
+void AddRandomConnection(struct Room** rooms_ptr)  
+{
+  struct Room* A;
+  struct Room* B;
 
   while(true)
   {
-    A = GetRandomRoom();
+    A = GetRandomRoom(rooms_ptr);
 
-    if (CanAddConnof strings stack overflowectionFrom(A) == true)
+    if (CanAddConnectionFrom(*A) == true)
       break;
   }
 
   do
   {
-    B = GetRandomRoom();
+    B = GetRandomRoom(rooms_ptr);
+  }  while(CanAddConnectionFrom(*B) == false || IsSameRoom(*A, *B) == true || ConnectionAlreadyExists(A, B) == true);
+ 
+  ConnectRoom(A, B); 
+  ConnectRoom(B, A);
+}
+
+// Assigns random Room names. Assign type 5 MID_ROOMS, START_ROOM, & END_ROOM.
+// Pre: Allocated array of 7 Room pointers
+// Post: 7 Randomly generated names and room types.
+void asnRooms (struct Room** new_rooms) {
+  char* names[] = {
+    "bleak",
+    "bitter",
+    "branding",
+    "tainted",
+    "eldritch",
+    "haunted",
+    "sentient",
+    "macabre",
+    "risky",
+    "rigid"
+  };
+  int picks = 10;
+
+  for(int i=0; i<7; i++){
+    int r = ( rand() % picks );
+    
+    new_rooms[i]->name = names[r];
+    new_rooms[i]->type = "MID_ROOM";
+
+    for( int j = r; j < picks; j++ ){
+      names[j] = names[j+1];
+    }
+    new_rooms[i]->doors = 0;
+    picks--;
   }
-  while(CanAddConnectionFrom(B) == false || IsSameRoom(A, B) == true || ConnectionAlreadyExists(A, B) == true);
-
-  ConnectRoom(A, B);  // TODO: Add this connection to the real variables, 
-  ConnectRoom(B, A);  //  because this A and B will be destroyed when this function terminates
+  new_rooms[0]->type = "START_ROOM";
+  new_rooms[6]->type = "END_ROOM";
 }
 
-// Returns a random Room, does NOT validate if connection can be added
-Room GetRandomRoom()
-{
-  ...
-}
 
-// Returns true if a connection can be added from Room x (< 6 outbound connections), false otherwise
-bool CanAddConnectionFrom(Room x) 
-{
-  ...
-}
-// Returns true if a connection from Room x to Room y already exists, false otherwise
-bool ConnectionAlreadyExists(x, y)
-{
-  ...
-}
+// Pre: Array of 7 assigned and connected rooms
+// Post: 7 files names ROOM_(1-7)
+void outputRooms (struct Room** final_rooms){
+  char num;
+  for(int i = 0; i < 7; i++){
+    
+    // Create directory
+    int p = getpid();
+    char directory[30];
+    sprintf(directory, "jerezam.rooms.%d\0", p);
+    mkdir(directory, 0777);
 
-// Connects Rooms x and y together, does not check if this connection is valid
-void ConnectRoom(Room x, Room y) 
-{
-  ...
-}
+    // File name
+    char path[6];
+    strcat(directory, "/ROOM_");
+    num = i + '1';
 
-// Returns true if Rooms x and y are the same Room, false otherwise
-bool IsSameRoom(Room x, Room y) 
-{
-  ...
+    // Get file
+    strncat(directory, &num, 1);
+    //strcat(directory, path);
+    FILE* output_file = fopen(directory, "w+");
+    char buffer[30];
+
+    // Write name
+    strcpy(buffer, "ROOM NAME: ");
+    strcat(buffer, final_rooms[i]->name);
+    strcat(buffer, "\n");
+    fwrite(buffer, sizeof(char), strlen(buffer), output_file);
+    
+    // Write connections
+    for(int d = 1; d < final_rooms[i]->doors+1; d++){
+      num = d + '0';
+      char* name = final_rooms[i]->connections[d-1]->name;
+      strcpy(buffer, "CONNECTION "); 
+      strncat(buffer, &num, 1 ); strcat(buffer, ": "); strcat(buffer, name); strcat(buffer, "\n");
+      fwrite(buffer, sizeof(char), strlen(buffer), output_file);
+    }
+
+    // Write room type
+    strcpy(buffer, "ROOM TYPE: ");
+    strcat(buffer, final_rooms[i]->type);
+    fwrite(buffer, sizeof(char), strlen(buffer), output_file);
+  }
 }
 
 void main () {
+
+  struct Room* graph[7];
+
+  // Generate Rooms
+  srand(time(0));
+  for(int i = 0; i < 7; i++){
+    graph[i]=malloc(sizeof(struct Room));
+  }
+  asnRooms(graph);
+
 	// Create all connections in graph
-	while (IsGraphFull() == false)
+  while (IsGraphFull(graph) == false)
 	{
-  		AddRandomConnection();
+      AddRandomConnection(graph); 
 	}
+
+  outputRooms(graph);
+  // Free allocated memory
+  for(int i = 0; i < 7; i++){
+    free(graph[i]);
+  }
 }
